af70ce22dfa7de55af52ebbd4571ae797aeca826
Hideki Ikemoto <ikemo333@gmail.com>
2018-10-09 22:11:25 +0900

    add <BR>

--- apps/content_script/block_anchor.js
+++ apps/content_script/block_anchor.js
@@ -6,25 +6,33 @@ class BlockAnchor {
         anchor.setAttribute("data-blocker-target-id", targetId);
         anchor.textContent = chrome.i18n.getMessage("blockThisPage");
         anchor.addEventListener("click", this.mediator.showBlockDialog.bind(this.mediator));
+        const br = document.createElement("br");
         div.appendChild(anchor);
+        div.appendChild(br);
         this.anchor = anchor;
+        this.br = br;
     }
     none() {
         this.anchor.style.display = "inline";
+        this.br.style.display = "inline";
     }
     unhide(blockReason) {
         if (blockReason.getType() !== BlockType.URL) {
             this.anchor.style.display = "inline";
+            this.br.style.display = "inline";
         }
         else {
             this.anchor.style.display = "none";
+            this.br.style.display = "none";
         }
     }
     hide() {
         this.anchor.style.display = "none";
+        this.br.style.display = "none";
     }
     block() {
         this.anchor.style.display = "none";
+        this.br.style.display = "none";
     }
 }
 //# sourceMappingURL=block_anchor.js.map
--- apps/content_script/block_anchor.ts
+++ apps/content_script/block_anchor.ts
@@ -1,5 +1,6 @@
 class BlockAnchor {
     private readonly anchor: HTMLAnchorElement;
+    private readonly br: HTMLBRElement;
     private readonly mediator: BlockMediator;
 
     constructor(mediator: BlockMediator, div: HTMLDivElement, targetId: string) {
@@ -11,28 +12,37 @@ class BlockAnchor {
         anchor.textContent = chrome.i18n.getMessage("blockThisPage");
         anchor.addEventListener("click", this.mediator.showBlockDialog.bind(this.mediator));
 
+        const br = document.createElement("br");
+
         div.appendChild(anchor);
+        div.appendChild(br);
 
         this.anchor = anchor;
+        this.br = br;
     }
 
     public none() {
         this.anchor.style.display = "inline";
+        this.br.style.display = "inline";
     }
 
     public unhide(blockReason: BlockReason) {
         if (blockReason.getType() !== BlockType.URL) {
             this.anchor.style.display = "inline";
+            this.br.style.display = "inline";
         } else {
             this.anchor.style.display = "none";
+            this.br.style.display = "none";
         }
     }
 
     public hide() {
         this.anchor.style.display = "none";
+        this.br.style.display = "none";
     }
 
     public block() {
         this.anchor.style.display = "none";
+        this.br.style.display = "none";
     }
 }
