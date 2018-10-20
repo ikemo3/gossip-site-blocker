b1b8644909d673c0098e05bc0f4cf3ac8afdb486
Hideki Ikemoto <ikemo333@gmail.com>
2018-10-20 22:16:17 +0900

    refactoring: extract function.

--- apps/content_script/block_target_factory.ts
+++ apps/content_script/block_target_factory.ts
@@ -6,6 +6,51 @@ interface IBlockable {
     getElement(): Element;
 }
 
+function blockGoogleElement(g1: Element,
+                            blockedSites: BlockedSites,
+                            bannedWords: IBannedWord[],
+                            idnOption: IAutoBlockIDNOption,
+                            defaultBlockType: string,
+                            id: string) {
+    const g = new GoogleElement(g1);
+
+    if (!g.canBlock()) {
+        return;
+    }
+
+    const blockState: BlockState = new BlockState(g, blockedSites, bannedWords, idnOption);
+
+    if (blockState.getState() === "hard") {
+        g.deleteElement();
+        return;
+    }
+
+    const mediator = new BlockMediator(g, blockState, id, defaultBlockType);
+}
+
+function blockGoogleInnerCard(g1: Element,
+                              blockedSites: BlockedSites,
+                              bannedWords: IBannedWord[],
+                              idnOption: IAutoBlockIDNOption,
+                              defaultBlockType: string,
+                              id: string) {
+    const g = new GoogleInnerCard(g1);
+
+    if (!g.canBlock()) {
+        return;
+    }
+
+    const blockState: BlockState = new BlockState(g, blockedSites, bannedWords, idnOption);
+
+    if (blockState.getState() === "hard") {
+        g.deleteElement();
+        return;
+    }
+
+    const mediator = new BlockMediator(g, blockState, id, defaultBlockType);
+    mediator.setWrappable("205px");
+}
+
 const BlockTargetFactory = {
     async init(blockedSites: BlockedSites,
                bannedWords: IBannedWord[],
@@ -14,40 +59,13 @@ const BlockTargetFactory = {
         let count = 0;
 
         document.querySelectorAll(".g").forEach((g1: Element) => {
-            const g = new GoogleElement(g1);
-
-            if (!g.canBlock()) {
-                return;
-            }
-
-            const blockState: BlockState = new BlockState(g, blockedSites, bannedWords, idnOption);
-
-            if (blockState.getState() === "hard") {
-                g.deleteElement();
-                return;
-            }
-
             const id = `block${++count}`;
-            const mediator = new BlockMediator(g, blockState, id, defaultBlockType);
+            blockGoogleElement(g1, blockedSites, bannedWords, idnOption, defaultBlockType, id);
         });
 
         document.querySelectorAll("g-inner-card").forEach((g1) => {
-            const g = new GoogleInnerCard(g1);
-
-            if (!g.canBlock()) {
-                return;
-            }
-
-            const blockState: BlockState = new BlockState(g, blockedSites, bannedWords, idnOption);
-
-            if (blockState.getState() === "hard") {
-                g.deleteElement();
-                return;
-            }
-
             const id = `block${++count}`;
-            const mediator = new BlockMediator(g, blockState, id, defaultBlockType);
-            mediator.setWrappable("205px");
+            blockGoogleInnerCard(g1, blockedSites, bannedWords, idnOption, defaultBlockType, id);
         });
 
         return this;
