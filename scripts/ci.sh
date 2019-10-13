f98ca29835a5aa7747967393055a7874b379aab3
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-13 10:21:01 +0900

    ci: migrate archive to scripts/ci.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,3 +3,11 @@
 yarn install
 yarn run tsc
 yarn test
+
+PACKAGE_NAME=$(jq -r .name package.json)
+echo "Archive ${PACKAGE_NAME}.crx"
+mkdir -p /tmp/workspace
+echo ${PEM_BASE64} | openssl enc -d -base64 -A > /tmp/${PACKAGE_NAME}.pem
+yarn crx pack -o /tmp/workspace/${PACKAGE_NAME}.crx apps -p /tmp/${PACKAGE_NAME}.pem
+
+jq .version_name apps/manifest.json | tr -d '"' > /tmp/workspace/version
