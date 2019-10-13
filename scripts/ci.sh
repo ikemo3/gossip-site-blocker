89a48d953c2c7d4a210b4fcfc46869d54186b2b8
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-13 14:19:13 +0900

    ci: use manifest's version for tag

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,5 +9,3 @@ echo "Archive ${PACKAGE_NAME}.crx"
 mkdir -p /tmp/workspace
 echo ${PEM_BASE64} | openssl enc -d -base64 -A > /tmp/${PACKAGE_NAME}.pem
 yarn crx pack -o /tmp/workspace/${PACKAGE_NAME}.crx apps -p /tmp/${PACKAGE_NAME}.pem
-
-jq .version_name apps/manifest.json | tr -d '"' > /tmp/workspace/version
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -e
 
+cd $(dirname $0)/..
+REPOSITORY_TOP=$(pwd)
+
+MANIFEST_VERSION=$(jq -r .version apps/manifest.json)
+echo "MANIFEST_VERSION: ${MANIFEST_VERSION}"
+
 if [ "${CI}" = "true" ]; then
   DO=
 else
   DO=echo
 fi
 
+cd /tmp/workspace
 ${DO} go get -u github.com/tcnksm/ghr
 
 ghr -t ${GITHUB_TOKEN} \
@@ -13,5 +20,5 @@ ghr -t ${GITHUB_TOKEN} \
     -r ${CIRCLE_PROJECT_REPONAME} \
     -c ${CIRCLE_SHA1} \
     --replace \
-    v$(cat /tmp/workspace/version) \
+    v${MANIFEST_VERSION} \
     /tmp/workspace/gossip-site-blocker.crx
