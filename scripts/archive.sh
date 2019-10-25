77b72fe2b0b34c2a74ea3952478573ee515f359f
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-25 22:02:32 +0900

    refactor(build): add/remove echos and comments

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -eu
 
 PACKAGE_NAME=$(jq -r .name package.json)
-echo "Archive ${PACKAGE_NAME}.crx"
+
+### create Chrome Extension
+# decode private key
 mkdir -p tmp/workspace
 echo ${PEM_BASE64} | openssl enc -d -base64 -A > tmp/${PACKAGE_NAME}.pem
+
+# create Chrome Extension(signed)
+echo "Create ${PACKAGE_NAME}.crx"
 yarn crx pack -o tmp/workspace/${PACKAGE_NAME}.crx apps -p tmp/${PACKAGE_NAME}.pem
+
+# create Chrome Extension(unsigned)
+echo "Create ${PACKAGE_NAME}.zip"
 yarn crx pack --zip-output tmp/workspace/${PACKAGE_NAME}.zip apps
