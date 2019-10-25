75173a6cd4d58f93fb53c1f1f101af9dfdcc4440
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-25 22:03:20 +0900

    build: set version_name for snapshot

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -eu
 
 PACKAGE_NAME=$(jq -r .name package.json)
+MANIFEST_VERSION=$(jq -r .version apps/manifest.json)
+
+# store manifest.json
+git checkout -- apps/manifest.json
+MANIFEST=$(cat apps/manifest.json)
 
 ### create Chrome Extension
 # decode private key
 mkdir -p tmp/workspace
 echo ${PEM_BASE64} | openssl enc -d -base64 -A > tmp/${PACKAGE_NAME}.pem
+
+# rewrite manifest.json
+if [[ "${CIRCLE_BRANCH}" != "" ]]; then
+  echo 'add `version_name` to manifest.json'
+  NOW=$(date +%Y%m%d-%H%M)
+  echo ${MANIFEST} | jq ". | .version_name = \"${MANIFEST_VERSION}-snapshot(${NOW})\"" \
+    > apps/manifest.json
+fi
 
 # create Chrome Extension(signed)
 echo "Create ${PACKAGE_NAME}.crx"
