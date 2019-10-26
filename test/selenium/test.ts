57b67eb62c194703129cabd6adfdbf96df3dcecf
Hideki Ikemoto <ikemo333@gmail.com>
2019-10-26 17:13:27 +0900

    style: reformat code

--- test/selenium/test.ts
+++ test/selenium/test.ts
@@ -14,31 +14,31 @@ async function takeScreenShot(driver: WebDriver, path: string): Promise<void> {
 }
 
 async function main(driver: WebDriver) {
-        await driver.get('https://www.google.com/search?q=typescript+wikipedia+site:ja.wikipedia.org');
-        await takeScreenShot(driver, 'search_result.png');
+    await driver.get('https://www.google.com/search?q=typescript+wikipedia+site:ja.wikipedia.org');
+    await takeScreenShot(driver, 'search_result.png');
 
-        // click 'block this page'
-        const blockThisPage = driver.findElement(By.className('block-google-element'));
-        const blockTarget = blockThisPage.findElement(By.xpath('preceding-sibling::div'));
-        await driver.actions().pause(500).click(blockThisPage).perform();
-        await takeScreenShot(driver, 'block_dialog.png');
+    // click 'block this page'
+    const blockThisPage = driver.findElement(By.className('block-google-element'));
+    const blockTarget = blockThisPage.findElement(By.xpath('preceding-sibling::div'));
+    await driver.actions().pause(500).click(blockThisPage).perform();
+    await takeScreenShot(driver, 'block_dialog.png');
 
-        // assert dialog
-        const blockDialog = driver.findElement(By.className('block-dialog'));
-        const domainRadio = blockDialog.findElement(By.id('blocker-dialog-domain-radio'));
-        strictEqual(await domainRadio.getAttribute('value'), 'ja.wikipedia.org');
+    // assert dialog
+    const blockDialog = driver.findElement(By.className('block-dialog'));
+    const domainRadio = blockDialog.findElement(By.id('blocker-dialog-domain-radio'));
+    strictEqual(await domainRadio.getAttribute('value'), 'ja.wikipedia.org');
 
-        // click block button
-        const blockButton = driver.findElement(By.className('blocker-primary-button'));
-        await driver.actions().pause(500).click(blockButton).perform();
-        await takeScreenShot(driver, 'block_clicked.png');
+    // click block button
+    const blockButton = driver.findElement(By.className('blocker-primary-button'));
+    await driver.actions().pause(500).click(blockButton).perform();
+    await takeScreenShot(driver, 'block_clicked.png');
 
-        // assert block target is hidden
-        const isDisplayed = await blockTarget.isDisplayed();
-        ok(!isDisplayed);
+    // assert block target is hidden
+    const isDisplayed = await blockTarget.isDisplayed();
+    ok(!isDisplayed);
 
-        // quit driver.
-        await driver.close();
+    // quit driver.
+    await driver.close();
 }
 
 (async (): Promise<void> => {
