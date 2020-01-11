cbb747914cc5c8bbd0a1ae9567618110f7b75ae9
Hideki Ikemoto <ikemo333@gmail.com>
2020-01-11 16:37:22 +0900

    build: create package if branch or pem is not found

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 PACKAGE_NAME=$(jq -r .name package.json)
 MANIFEST_VERSION=$(jq -r .version dist/manifest.json)
@@ -8,10 +8,6 @@ git checkout -- apps/manifest.json
 MANIFEST=$(cat apps/manifest.json)
 
 ### create Chrome Extension
-# decode private key
-mkdir -p tmp/workspace
-echo ${PEM_BASE64} | openssl enc -d -base64 -A > tmp/${PACKAGE_NAME}.pem
-
 # rewrite manifest.json
 if [[ "${CIRCLE_BRANCH}" != "" ]]; then
   echo 'add `version_name` to manifest.json'
@@ -20,9 +16,19 @@ if [[ "${CIRCLE_BRANCH}" != "" ]]; then
     > dist/manifest.json
 fi
 
-# create Chrome Extension(signed)
-echo "Create ${PACKAGE_NAME}.crx"
-yarn crx pack -o tmp/workspace/${PACKAGE_NAME}.crx dist -p tmp/${PACKAGE_NAME}.pem
+# decode private key
+mkdir -p tmp/workspace
+
+if [ "${PEM_BASE64}" != "" ]; then
+  echo ${PEM_BASE64} | openssl enc -d -base64 -A > tmp/${PACKAGE_NAME}.pem
+
+  # create Chrome Extension(signed)
+  echo "Create ${PACKAGE_NAME}.crx"
+  yarn crx pack -o tmp/workspace/${PACKAGE_NAME}.crx dist -p tmp/${PACKAGE_NAME}.pem
+else
+  # create Chrome Extension(signed, create private key)
+  yarn crx pack -o tmp/workspace/${PACKAGE_NAME}.crx dist
+fi
 
 # create Chrome Extension(unsigned)
 echo "Create ${PACKAGE_NAME}.zip"
