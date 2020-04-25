bdef7a9b14fa6e493e0021b842033ebc13b85fb5
Hideki Ikemoto <ikemo333@gmail.com>
2020-04-25 23:07:28 +0900

    refactor: get screenshot dir from file name

--- test/selenium/test_google_search_result.ts
+++ test/selenium/test_google_search_result.ts
@@ -4,12 +4,12 @@ import TestBlockAnchor from './libs/block_anchor';
 
 export default async function main(driver: TestWebDriver): Promise<void> {
     await driver.googleSearch(['typescript', 'wikipedia', 'site:ja.wikipedia.org']);
-    await driver.takeScreenShot('test_google_search_result', 'search_result.png');
+    await driver.takeScreenShot('search_result.png');
 
     // click 'block this page'
     const blockAnchor = new TestBlockAnchor(driver, '.block-google-element');
     await blockAnchor.click();
-    await driver.takeScreenShot('test_google_search_result', 'block_dialog.png');
+    await driver.takeScreenShot('block_dialog.png');
 
     // assert dialog
     const blockDialog = await driver.findDialog();
@@ -17,7 +17,7 @@ export default async function main(driver: TestWebDriver): Promise<void> {
 
     // click block button
     await blockDialog.block();
-    await driver.takeScreenShot('test_google_search_result', 'block_clicked.png');
+    await driver.takeScreenShot('block_clicked.png');
 
     // assert block target is hidden
     const blockTarget = await blockAnchor.getTarget('preceding-sibling::div');
--- test/selenium/test_google_search_top_news.ts
+++ test/selenium/test_google_search_top_news.ts
@@ -4,12 +4,12 @@ import TestBlockAnchor from './libs/block_anchor';
 
 export default async function main(driver: TestWebDriver): Promise<void> {
     await driver.googleSearch(['switch', 'site:game.watch.impress.co.jp']);
-    await driver.takeScreenShot('test_google_search_top_news', 'search_result.png');
+    await driver.takeScreenShot('search_result.png');
 
     // click 'block this page'
     const blockAnchor = new TestBlockAnchor(driver, '.block-google-top-news');
     await blockAnchor.click();
-    await driver.takeScreenShot('test_google_search_top_news', 'block_dialog.png');
+    await driver.takeScreenShot('block_dialog.png');
 
     // assert dialog
     const blockDialog = await driver.findDialog();
@@ -17,7 +17,7 @@ export default async function main(driver: TestWebDriver): Promise<void> {
 
     // click block button
     await blockDialog.block();
-    await driver.takeScreenShot('test_google_search_top_news', 'block_clicked.png');
+    await driver.takeScreenShot('block_clicked.png');
 
     // assert block target is hidden
     const blockTarget = await blockAnchor.getTarget('preceding-sibling::div');
