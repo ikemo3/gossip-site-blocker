305841051b8209fab5a1f70f8fd0214df21b7eb8
Hideki Ikemoto <ikemo333@gmail.com>
2021-07-22 18:18:33 +0900

    fix: block links are not displayed in some environments

---
+++ test/google_search_url.test.ts
@@ -0,0 +1,24 @@
+import GoogleSearchURL from "../apps/values/google_search_url";
+
+describe("GoogleSearchURL", () => {
+    it("getURLParameter", () => {
+        const url = new GoogleSearchURL(
+            "/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=def&url=https%3A%2F%2Fexample.com%2Ffoo&usg=abc"
+        );
+        expect(url.getURLParameter()).toBe("https://example.com/foo");
+    });
+
+    it("getURLParameter(no usg)", () => {
+        const url = new GoogleSearchURL(
+            "/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=def&url=https%3A%2F%2Fexample.com%2Ffoo"
+        );
+        expect(url.getURLParameter()).toBe("https://example.com/foo");
+    });
+
+    it("getURLParameter(no url)", () => {
+        const url = new GoogleSearchURL(
+            "/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=def&urx=https%3A%2F%2Fexample.com%2Ffoo&usg=abc"
+        );
+        expect(url.getURLParameter()).toBe(null);
+    });
+});
