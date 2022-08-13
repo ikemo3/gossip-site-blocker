3c8323c517ad6a2e2a7f491e49705dac4bf80783
Hideki Ikemoto <ikemo333@gmail.com>
2022-08-13 17:20:41 +0900

    package: replace scripts

--- scripts/make_manifest.ts
+++ scripts/make_manifest.ts
@@ -1,3 +1,26 @@
-import { createManifest } from "./libs";
+import path from "path";
+import { mkdirSync, existsSync, readFileSync, writeFileSync } from "fs";
+import { format } from "date-fns";
 
-createManifest();
+// Create if there is no dist directory
+const projectDir = path.dirname(__dirname);
+const distDir = projectDir + "/dist";
+const manifestJsonPath = projectDir + "/apps/manifest.json";
+
+if (!existsSync(distDir)) {
+    mkdirSync(distDir);
+}
+
+// Load manifest.json
+const manifest = JSON.parse(readFileSync(manifestJsonPath).toString());
+
+// set version_name
+const branch = process.env.CIRCLE_BRANCH;
+if (branch) {
+    const version = manifest.version;
+    const now = format(new Date(), "yyyyMMdd-HHmm");
+    manifest.version_name = `${version}-snapshot(${now})`;
+}
+
+const manifestWriteJsonPath = distDir + "/manifest.json";
+writeFileSync(manifestWriteJsonPath, JSON.stringify(manifest, null, 2));
