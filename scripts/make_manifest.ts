6f7fa36a1d47c5fd92eecf28ee5d5850c954caf2
Hideki Ikemoto <ikemo333@gmail.com>
2022-10-06 19:45:45 +0900

    refactor: separate manifest.json

--- scripts/make_manifest.ts
+++ scripts/make_manifest.ts
@@ -6,6 +6,7 @@ import { format } from "date-fns";
 const projectDir = path.dirname(__dirname);
 const distDir = projectDir + "/dist";
 const manifestJsonPath = projectDir + "/apps/manifest.json";
+const firefoxManifestJsonPath = projectDir + "/apps/manifest.firefox.json";
 
 if (!existsSync(distDir)) {
     mkdirSync(distDir);
@@ -13,6 +14,7 @@ if (!existsSync(distDir)) {
 
 // Load manifest.json
 const manifest = JSON.parse(readFileSync(manifestJsonPath).toString());
+const firefoxManifest = JSON.parse(readFileSync(firefoxManifestJsonPath).toString());
 
 // set version_name
 const branch = process.env.CIRCLE_BRANCH;
@@ -20,7 +22,11 @@ if (branch) {
     const version = manifest.version;
     const now = format(new Date(), "yyyyMMdd-HHmm");
     manifest.version_name = `${version}-snapshot(${now})`;
+    firefoxManifest.version_name = `${version}-snapshot(${now})`;
 }
 
 const manifestWriteJsonPath = distDir + "/manifest.json";
 writeFileSync(manifestWriteJsonPath, JSON.stringify(manifest, null, 2));
+
+const firefoxManifestWriteJsonPath = distDir + "/manifest.firefox.json";
+writeFileSync(firefoxManifestWriteJsonPath, JSON.stringify(firefoxManifest, null, 2));
