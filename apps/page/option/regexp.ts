f15497f8588c88e42afa1acd4c8a6ee37fcf8ecb
vibhor <vibh1103@gmail.com>
2023-10-02 22:28:13 +0530

    Remove as keyword

--- apps/page/option/regexp.ts
+++ apps/page/option/regexp.ts
@@ -4,31 +4,33 @@ import { ApplicationError } from "../../libs/error";
 import { BlockType } from "../../storage/enums";
 
 class RegExpList {
-  private readonly regexpList: HTMLDivElement;
+  private readonly regexpList;
 
-  private readonly addText: HTMLInputElement;
+  private readonly addText;
 
-  private readonly addButton: HTMLInputElement;
+  private readonly addButton;
 
   constructor() {
-    this.regexpList = document.getElementById("regexpList") as HTMLDivElement;
-    this.addText = document.getElementById("regexpAddText") as HTMLInputElement;
-    this.addButton = document.getElementById(
-      "regexpAddButton",
-    ) as HTMLInputElement;
-    $.onclick(this.addButton, this.addItem.bind(this));
+    this.regexpList = document.getElementById("regexpList");
+    this.addText = document.getElementById("regexpAddText");
+    this.addButton = document.getElementById("regexpAddButton");
+    if (this.addButton instanceof HTMLInputElement) {
+      $.onclick(this.addButton, this.addItem.bind(this));
+    }
   }
 
   public async load(): Promise<void> {
     const patternList = await RegExpRepository.load();
     for (const pattern of patternList) {
       const itemDiv = this.createItem(pattern);
-      this.regexpList.appendChild(itemDiv);
+      this.regexpList?.appendChild(itemDiv);
     }
   }
 
   public clear(): void {
-    this.regexpList.innerHTML = "";
+    if (this.regexpList instanceof HTMLDivElement) {
+      this.regexpList.innerHTML = "";
+    }
   }
 
   private createItem(item: RegExpItem): HTMLDivElement {
@@ -58,46 +60,50 @@ class RegExpList {
   }
 
   private async addItem(): Promise<void> {
-    const pattern = this.addText.value;
-    if (pattern === "") {
-      return;
-    }
+    if (this.addText instanceof HTMLInputElement) {
+      const pattern = this.addText.value;
+      if (pattern === "") {
+        return;
+      }
 
-    const regexp = $.regexp(pattern);
-    if (regexp === null) {
-      // eslint-disable-next-line no-alert
-      alert($.message("invalidPattern"));
-      return;
-    }
+      const regexp = $.regexp(pattern);
+      if (regexp === null) {
+        // eslint-disable-next-line no-alert
+        alert($.message("invalidPattern"));
+        return;
+      }
 
-    const added = await RegExpRepository.add(pattern);
-    if (added) {
-      // add item
-      const item = await this.createItem({
-        pattern,
-        blockType: BlockType.SOFT,
-      });
-      this.regexpList.appendChild(item);
-    }
+      const added = await RegExpRepository.add(pattern);
+      if (added) {
+        // add item
+        const item = await this.createItem({
+          pattern,
+          blockType: BlockType.SOFT,
+        });
+        this.regexpList?.appendChild(item);
+      }
 
-    // clear text
-    this.addText.value = "";
+      // clear text
+      this.addText.value = "";
+    }
   }
 
   private async changeType(pattern: string, ev: Event): Promise<void> {
-    const typeSelect: HTMLSelectElement = ev.target as HTMLSelectElement;
-    const index = typeSelect.selectedIndex;
-    const { value } = typeSelect.options[index];
+    const typeSelect = ev.target;
+    if (typeSelect instanceof HTMLSelectElement) {
+      const index = typeSelect.selectedIndex;
+      const { value } = typeSelect.options[index];
 
-    switch (value) {
-      case "soft":
-        await RegExpRepository.changeType(pattern, BlockType.SOFT);
-        break;
-      case "hard":
-        await RegExpRepository.changeType(pattern, BlockType.HARD);
-        break;
-      default:
-        throw new ApplicationError(`unknown value:${value}`);
+      switch (value) {
+        case "soft":
+          await RegExpRepository.changeType(pattern, BlockType.SOFT);
+          break;
+        case "hard":
+          await RegExpRepository.changeType(pattern, BlockType.HARD);
+          break;
+        default:
+          throw new ApplicationError(`unknown value:${value}`);
+      }
     }
   }
 
